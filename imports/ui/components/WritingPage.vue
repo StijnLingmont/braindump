c2945035438dfec095f817fccec837c0759e12ca
Stijn Lingmont <s.lingmont@student.avans.nl>
2022-04-02 20:22:27 +0200

    Adding lists

--- imports/ui/components/WritingPage.vue
+++ imports/ui/components/WritingPage.vue
@@ -1,11 +1,31 @@
 <template>
   <div class="writing-page">
-    <div class="writing-block" contenteditable="true"></div>
+    <div class="writing-block" @keyup.space="runCommand" contenteditable="true">
+      <div>
+        <br>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "WritingPage"
+  name: "WritingPage",
+  methods: {
+    runCommand() {
+      var usingContentBlock = document.getSelection().anchorNode;
+      var content = usingContentBlock.textContent;
+
+      if(content.startsWith("-")) {
+        document.execCommand("insertUnorderedList", false, "");
+        document.getSelection().anchorNode.textContent = usingContentBlock.textContent.slice(1);
+      }
+
+      if(content.startsWith("1.")) {
+        document.execCommand("insertOrderedList", false, "");
+        document.getSelection().anchorNode.textContent = usingContentBlock.textContent.slice(1);
+      }
+    }
+  }
 }
 </script>
